90e9952ffeb704768a08c84905341d0a05390234
nshuster7 <nshuster@g.jct.ac.il>
2024-11-02 21:37:53 +0200

    change the desgin

--- public/js/homeImages.js
+++ public/js/homeImages.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const heroSection = document.querySelector('.hero-section .background');
     const images = [
         'https://domf5oio6qrcr.cloudfront.net/medialibrary/13503/bigstock-Health-food-selection-super-foods-fruits-veggies.jpg',
-        'https://www.apa.org/images/2023-06-continuing-education-nutrition-tile_tcm7-316328.jpg',
+        'https://klinio.com/blog/app/uploads/2021/06/5zS8XZHkJbWCKU9UgYGEf8FoMqY7dv6MI8iMbnmy.png',
         'https://images.squarespace-cdn.com/content/v1/59f0e6beace8641044d76e9c/1572359931877-CE6Q9BP8DCXEWCV18FIY/Nutrition+Basics.jpeg'
     ];
     
